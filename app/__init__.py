23a367702714aee28b1409f41c0f3472fbc5d71c
andorafa <ando4rafa@gmail.com>
2025-04-27 18:22:22 +0200

    Fix: correction import Flask dans app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 # app/__init__.py
-from flask import Flask, app
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 db = SQLAlchemy()
-
 migrate = Migrate()
 
 def create_app():
@@ -22,25 +21,17 @@ def create_app():
     from flask_restful import Api
     api = Api(app)
 
-
-    
-    # Importer et enregistrer les ressources (endpoints)
+    # Importer et enregistrer les ressources
     from app.resources.webshop import WebshopAPI
     from app.resources.revendeurs import RevendeursAPI
-    
-    api.add_resource(WebshopAPI, '/api/webshop/products')
-    api.add_resource(RevendeursAPI, '/api/revendeurs/products')
-    
-    
-    # Ajout du nouvel endpoint pour la gestion des utilisateurs
     from app.resources.user import UserAPI
     from app.resources.authenticate import AuthenticateAPI
+    from app.resources.logout import LogoutAPI
+
+    api.add_resource(WebshopAPI, '/api/webshop/products')
+    api.add_resource(RevendeursAPI, '/api/revendeurs/products')
     api.add_resource(UserAPI, '/api/users')
     api.add_resource(AuthenticateAPI, '/api/revendeurs/authenticate')
-
-    #deconnexion
-    from app.resources.logout import LogoutAPI
-    api.add_resource(LogoutAPI, '/api/logout')  # endpoint de déconnexion
-
+    api.add_resource(LogoutAPI, '/api/logout')
 
     return app
