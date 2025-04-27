e8f5fa5b8d3d2e0a8074ac9cba5cd96e0d8896a2
andorafa <ando4rafa@gmail.com>
2025-04-27 17:04:27 +0200

    Fix: chemin sys.path pour import Flask dans GitHub Actions

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 # run.py
+import os
+import sys
 from app import create_app
+
+
+# 👉 Ajouter le dossier parent au chemin système
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 app = create_app()
 
