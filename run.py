6d1f37a4ff68f88ad7c1ecdf8fffa2e7bdc040fd
andorafa <ando4rafa@gmail.com>
2025-04-27 18:52:43 +0200

    Fix: create tables automatically on startup

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 # run.py
 
-
 from app import create_app
+from app import db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-    print("🚀 Flask server starting on 0.0.0.0:5000...")
     app.run(host="0.0.0.0", port=5000)
 
