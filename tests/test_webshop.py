608be53e8eaf99d7e4b2eed9f110eb36def61f26
andorafa <ando4rafa@gmail.com>
2025-06-11 01:25:34 +0200

    ✅ Fix test_webshop by inserting test user with api_key

--- tests/test_webshop.py
+++ tests/test_webshop.py
@@ -1,9 +1,15 @@
 import pytest
 from app import create_app, db
 from app.config import Config
+from app.models import User
 
 
 def test_webshop_valid_key(client):
+    with client.application.app_context():
+        # Crée un utilisateur avec la bonne clé
+        db.session.add(User(email="test@webshop.com", api_key="webshop123"))
+        db.session.commit()
+
     response = client.get("/api/webshop/products", headers={"x-api-key": "webshop123"})
     assert response.status_code == 200
 
