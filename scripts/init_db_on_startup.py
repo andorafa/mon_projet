4fca3f5e2337890cd88abf41294ee1379bc1eca7
andorafa <ando4rafa@gmail.com>
2025-06-24 21:00:19 +0200

    Fix path import for Render pre-deploy

--- scripts/init_db_on_startup.py
+++ scripts/init_db_on_startup.py
@@ -1,5 +1,10 @@
 # scripts/init_db_on_startup.py
 import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import create_app, db
 from app.models import Product
 from sqlalchemy.exc import OperationalError
