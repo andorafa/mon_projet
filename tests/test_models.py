1fa60c9f15b16f47908aa4387f965a6328c32aa5
andorafa <ando4rafa@gmail.com>
2025-06-24 21:45:24 +0200

    Fix test_user_repr avec nom et prénom

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,4 +6,5 @@ def test_product_repr():
 
 def test_user_repr():
     user = User(email="toto@test.com", api_key="xyz", first_name="Toto", last_name="Test")
-    assert repr(user) == "<User toto@test.com>"
+    assert repr(user) == "<User Toto Test (toto@test.com)>"
+
