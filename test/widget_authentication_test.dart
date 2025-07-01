7cc2d824965cd19dd8ab6eda6ffd76cba2904174
andorafa <ando4rafa@gmail.com>
2025-07-01 15:46:10 +0200

    Fix test widget_authentication_test.dart to find email TextField by semantics label

--- test/widget_authentication_test.dart
+++ test/widget_authentication_test.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test_app/pages/authentication_page.dart';
 
-
 void main() {
   testWidgets('Affiche le champ email et les boutons', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: AuthenticationPage()));
 
-    expect(find.text('Email'), findsOneWidget);
+    expect(find.bySemanticsLabel('Prénom'), findsOneWidget);
+    expect(find.bySemanticsLabel('Nom'), findsOneWidget);
+    expect(find.bySemanticsLabel('Email'), findsOneWidget); // ✅ corrigé ici
     expect(find.text("S'inscrire et recevoir le QR Code"), findsOneWidget);
     expect(find.text('Se connecter (scanner QR Code)'), findsOneWidget);
   });
